b7250f29cdea592b5cda773b8c89b35a46fb78b3
Bartosz Boryn <bartosz.boryn@gmail.com>
2016-07-24 21:47:37 +0200

    Solverd hard task

--- wyzwanie2/hard.py
+++ wyzwanie2/hard.py
@@ -1,14 +1,50 @@
+import os
+import hashlib
+import sys
+
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine("sqlite:///test.db")
+def check_entry_type(entry):
+    if entry.is_symlink() is True:
+        return "o"
+    elif entry.is_file() is True:
+        return "f"
+    elif entry.is_dir() is True:
+        return "d"
+    else:
+        return "o"
+
+
+def count_directory_contents(entry):
+    directory_contents = os.scandir(entry.path)
+    count = 0
+    for element in directory_contents:
+        count += 1
+    return count
+
+
+def get_directory_size(entry):
+    directory_contents = os.scandir(entry.path)
+    size_count = 0
+    for item in directory_contents:
+        size_count += item.stat().st_size
+    return size_count
+
+
+def get_checksum(entry):
+    return hashlib.md5(open(entry.path, 'rb').read()).hexdigest()
+
+
 Base = declarative_base()
 
 
 class Entry(Base):
-    __tablename__ = "Objects"
+
+    __tablename__ = "objects"
 
     id = Column(Integer, primary_key=True)
     path = Column(String)
@@ -16,4 +52,49 @@ class Entry(Base):
     size = Column(Integer)
 
 
+class Cardinality(Base):
+
+    __tablename__ = 'cardinality'
+
+    id = Column(Integer, ForeignKey("objects.id"), primary_key=True, autoincrement=False)
+    nbr_of_elements = Column(Integer)
+
+
+class Checksum(Base):
+
+    __tablename__ = 'checksums'
+
+    id = Column(Integer, ForeignKey("objects.id"), primary_key=True, autoincrement=False)
+    checksum = Column(String)
+
+
+engine = create_engine("sqlite:///" + sys.argv[2] + ".db")
 Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+directory_contents = os.scandir(sys.argv[1])
+for entry in directory_contents:
+    entry_type = check_entry_type(entry)
+    if entry_type == "d":
+        item = Entry(path=entry.path, type=entry_type, size=get_directory_size(entry))
+        session.add(item)
+        session.commit()
+        session.add(Cardinality(id=session.query(Entry).filter_by(path=entry.path).first().id,
+                                nbr_of_elements=count_directory_contents(entry)))
+        session.commit()
+    elif entry_type == "f":
+        item = Entry(path=entry.path, type=entry_type, size=entry.stat().st_size)
+        session.add(item)
+        session.commit()
+        session.add(Checksum(id=session.query(Entry).filter_by(path=entry.path).first().id,
+                             checksum=get_checksum(entry)))
+    else:
+        item = Entry(path=entry.path, type=entry_type, size=entry.stat().st_size)
+        session.add(item)
+        session.commit()
+
+
+
+
